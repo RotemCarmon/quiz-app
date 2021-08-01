2b460db016ab350b2636d8068ea00e8cc3391aed
Roi Levy <levy6161@gmail.com>
2021-08-01 14:17:43 +0300

    minor

--- quizz-app/src/views/quiz.vue
+++ quizz-app/src/views/quiz.vue
@@ -18,67 +18,67 @@
 export default {
   data() {
     return {
-    //   quiz: {
-    //     _id: "t101",
-    //     title: "Quiz title",
-    //     sections: ["js", "vue", "react"],
-    //     qusts: [
-    //       {
-    //         id: "q101",
-    //         section: "vue",
-    //         qust: "Question 1",
-    //         opts: [
-    //           {
-    //             id: "o101",
-    //             txt: "Option 1",
-    //             imgUrl: "http:// fhdjsks.com",
-    //             isCorrect: true,
-    //           },
-    //           { id: "o102", txt: "Option 2", imgUrl: "http:// fhdjsks.com" },
-    //           { id: "o103", txt: "Option 3", imgUrl: "http:// fhdjsks.com" },
-    //           { id: "o104", txt: "Option 4", imgUrl: "http:// fhdjsks.com" },
-    //         ],
-    //       },
-    //       {
-    //         id: "q102",
-    //         section: "vue",
-    //         qust: "Question 2",
-    //         opts: [
-    //           {
-    //             id: "o101",
-    //             txt: "Option 1",
-    //             imgUrl: "http:// fhdjsks.com",
-    //             isCorrect: true,
-    //           },
-    //           { id: "o102", txt: "Option 2", imgUrl: "http:// fhdjsks.com" },
-    //           { id: "o103", txt: "Option 3", imgUrl: "http:// fhdjsks.com" },
-    //           { id: "o104", txt: "Option 4", imgUrl: "http:// fhdjsks.com" },
-    //         ],
-    //       },
-    //       {
-    //         id: "q103",
-    //         section: "vue",
-    //         qust: "Question 3",
-    //         opts: [
-    //           {
-    //             id: "o101",
-    //             txt: "Option 1",
-    //             imgUrl: "http:// fhdjsks.com",
-    //             isCorrect: true,
-    //           },
-    //           { id: "o102", txt: "Option 2", imgUrl: "http:// fhdjsks.com" },
-    //           { id: "o103", txt: "Option 3", imgUrl: "http:// fhdjsks.com" },
-    //           { id: "o104", txt: "Option 4", imgUrl: "http:// fhdjsks.com" },
-    //         ],
-    //       },
-    //     ],
-    //   },
+      quiz: {
+        _id: "t101",
+        title: "Quiz title",
+        sections: ["js", "vue", "react"],
+        qusts: [
+          {
+            id: "q101",
+            section: "vue",
+            qust: "Question 1",
+            opts: [
+              {
+                id: "o101",
+                txt: "Option 1",
+                imgUrl: "http:// fhdjsks.com",
+                isCorrect: true,
+              },
+              { id: "o102", txt: "Option 2", imgUrl: "http:// fhdjsks.com" },
+              { id: "o103", txt: "Option 3", imgUrl: "http:// fhdjsks.com" },
+              { id: "o104", txt: "Option 4", imgUrl: "http:// fhdjsks.com" },
+            ],
+          },
+          {
+            id: "q102",
+            section: "vue",
+            qust: "Question 2",
+            opts: [
+              {
+                id: "o101",
+                txt: "Option 1",
+                imgUrl: "http:// fhdjsks.com",
+                isCorrect: true,
+              },
+              { id: "o102", txt: "Option 2", imgUrl: "http:// fhdjsks.com" },
+              { id: "o103", txt: "Option 3", imgUrl: "http:// fhdjsks.com" },
+              { id: "o104", txt: "Option 4", imgUrl: "http:// fhdjsks.com" },
+            ],
+          },
+          {
+            id: "q103",
+            section: "vue",
+            qust: "Question 3",
+            opts: [
+              {
+                id: "o101",
+                txt: "Option 1",
+                imgUrl: "http:// fhdjsks.com",
+                isCorrect: true,
+              },
+              { id: "o102", txt: "Option 2", imgUrl: "http:// fhdjsks.com" },
+              { id: "o103", txt: "Option 3", imgUrl: "http:// fhdjsks.com" },
+              { id: "o104", txt: "Option 4", imgUrl: "http:// fhdjsks.com" },
+            ],
+          },
+        ],
+      },
     };
   },
   async created() {
-    const { quizId } = this.$route.params;
-    const quiz = await this.$store.dispatch("loadQuiz", quizId);
-    this.quiz = quiz;
+    // const { quizId } = this.$route.params;
+    // const quiz = await this.$store.dispatch("loadQuiz", quizId);
+    // this.quiz = quiz;
   },
 };
 </script>
